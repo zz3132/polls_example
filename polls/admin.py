92ed4441bc7aaf0bb454374637b6e763295be382
zz3132 <zz3132@hs.ac.kr>
2022-05-11 15:09:30 +0900

    Initial commit

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from polls.models import Question, Choice
 #polls 안에있는 모델.py에서 question, choice 함수 불러온거
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice #초이스 모델을 사용
+    extra = 2
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [('Question Statement', {'fields' : ['question_text']}),
+              ('Date Information', {'fields' : ['pub_date'], 'classes':['collapse']})]
+    inlines = [ChoiceInline] #적용된 초이스모델을 question admin 클래스 안에 넣어줘서 같은 화면에 표시되게 함
+                            #실제로 값이 변경된것이 아니라 같은 구성에서 출력만 바꿔놓음
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
