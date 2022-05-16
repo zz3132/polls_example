e0d0a19ed36c0704eb9cbac36785b75256c56438
zz3132 <zz3132@hs.ac.kr>
2022-05-16 18:32:07 +0900

    Initial commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 # Register your models here.
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 2
 class QuestionAdmin(admin.ModelAdmin):
