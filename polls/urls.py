cd545597b5022c5759a98639ba5722e66cffb5b4
zz3132 <zz3132@hs.ac.kr>
2022-05-18 15:42:01 +0900

    Initial commit

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name='polls'
 
-urlpatterns=[
+urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
